ee0a9f1c536749cc4f5baef59c17b4040479ed4c
Nathan Szurek <nathan.szurek@yahoo.com>
2020-08-24 08:44:11 -0500

    Heroku troubleshoot

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
-const PORT = 8080 || process.env.PORT
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.sendFile('index.html')
-// })
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
-})
+});
